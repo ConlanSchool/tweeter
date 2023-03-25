7172e3474cdce9f9b0e0a51028fb8327f3457f89
ConlanSchool <conlan.school@gmail.com>
2023-03-24 17:20:03 -0700

    Add functionality to display multiple tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// app.post("/tweets")
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
+
+const renderTweets = function (arrOfTweets) {
+  for (let tweet of arrOfTweets) {
+    let value = createTweetElement(tweet);
+    $(".tweets-container").append(value);
+  }
+};
+
+const createTweetElement = function (tweet) {
+  let $tweet = $(`
+    <section class="posted-tweet">
+      <article>
+        <header>
+          <img src="${tweet.user.avatars}">
+          <h3>${tweet.user.name}</h3>
+          <h4>${tweet.user.handle}</h4>
+        </header>
+        <p>
+          ${tweet.content.text}
+        </p>
+        <footer>
+          ${tweet.user.created_at}
+          <div class="icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-sharp fa-solid fa-repeat"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    </section>
+  `);
+  return $tweet;
+};
+
+$(document).ready(function () {
+  renderTweets(data);
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-  console.log("Tex!");
-
   $(".new-tweet form textarea").on("input", function (event) {
     let charCount = 140 - $(this).val().length;
     $(".new-tweet form .counter").text(charCount);
