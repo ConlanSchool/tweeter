c6585c98041075b4a929146ad402fa22adcceb86
ConlanSchool <conlan.school@gmail.com>
2023-03-26 17:38:48 -0700

    Add posting new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,65 +5,67 @@
  */
 
 // app.post("/tweets")
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
-const renderTweets = function (arrOfTweets) {
-  for (let tweet of arrOfTweets) {
-    let value = createTweetElement(tweet);
-    $(".tweets-container").append(value);
-  }
-};
-
-const createTweetElement = function (tweet) {
-  let $tweet = $(`
-    <section class="posted-tweet">
-      <article>
-        <header>
-          <img src="${tweet.user.avatars}">
-          <h3>${tweet.user.name}</h3>
-          <h4>${tweet.user.handle}</h4>
-        </header>
-        <p>
-          ${tweet.content.text}
-        </p>
-        <footer>
-          ${timeago.format(tweet.created_at)}
-          <div class="icons">
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-sharp fa-solid fa-repeat"></i>
-            <i class="fa-solid fa-heart"></i>
-          </div>
-        </footer>
-      </article>
-    </section>
-  `);
-  return $tweet;
-};
-
 $(document).ready(function () {
+  const renderTweets = function (arrOfTweets) {
+    //Clear feed on refresh or initial load
+    console.log(arrOfTweets);
+
+    $(".tweets-container").empty();
+
+    //Loop through tweets array and send them to be formatted with html
+    for (let tweet of arrOfTweets) {
+      let value = createTweetElement(tweet);
+      $(".tweets-container").prepend(value);
+    }
+  };
+
+  //Formats tweet data into html
+  const createTweetElement = function (tweet) {
+    if (!tweet.user || !tweet.user.avatars) {
+      console.error("Invalid tweet object:", tweet);
+      return null;
+    }
+
+    let $tweet = $(`
+      <section class="posted-tweet">
+        <article>
+          <header>
+            <img src="${tweet.user.avatars}">
+            <h3>${tweet.user.name}</h3>
+            <h4>${tweet.user.handle}</h4>
+          </header>
+          <p>
+            ${tweet.content.text}
+          </p>
+          <footer>
+            ${timeago.format(tweet.created_at)}
+            <div class="icons">
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-sharp fa-solid fa-repeat"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+      </section>
+    `);
+    return $tweet;
+  };
+
+  const loadNewTweet = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json",
+    })
+      .then(function (data) {
+        renderTweets(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert("An error occurred while loading tweets.");
+      });
+  };
+
   $("form").submit(function (event) {
     event.preventDefault();
 
@@ -81,18 +83,15 @@ $(document).ready(function () {
         data: formData,
       })
         .then(function (response) {
-          console.log(response);
+          loadNewTweet(response);
+
+          // clear the tweet input field
+          $("textarea").val("");
         })
         .catch(function (error) {
           console.log(error);
           alert("An error occurred while submitting the form.");
         });
     }
-  });
-
-  $(function () {
-    $.get("/tweets", function (data) {
-      renderTweets(data);
-    });
   });
 });
--- public/scripts/tweet.js
+++ public/scripts/tweet.js
@@ -1,45 +1,29 @@
-//Tweet hover
 $(document).ready(function () {
-  $(".posted-tweet").hover(
-    function () {
-      $(this).css("box-shadow", "0 4px 10px rgba(0, 0, 0, 0.5)");
-    },
-    function () {
-      $(this).css("box-shadow", "none");
-    }
-  );
-});
+  $(document).on("mouseenter", ".posted-tweet", function () {
+    $(this).css("box-shadow", "0 4px 10px rgba(0, 0, 0, 0.5)");
+  });
+  $(document).on("mouseleave", ".posted-tweet", function () {
+    $(this).css("box-shadow", "none");
+  });
 
-//Icon hover
-$(document).ready(function () {
-  $(".fa-solid.fa-flag").hover(
-    function () {
-      $(this).css("color", "gold");
-    },
-    function () {
-      $(this).css("color", "");
-    }
-  );
-});
+  $(document).on("mouseenter", ".fa-solid.fa-flag", function () {
+    $(this).css("color", "gold");
+  });
+  $(document).on("mouseleave", ".fa-solid.fa-flag", function () {
+    $(this).css("color", "");
+  });
 
-$(document).ready(function () {
-  $(".fa-sharp.fa-solid.fa-repeat").hover(
-    function () {
-      $(this).css("color", "gold");
-    },
-    function () {
-      $(this).css("color", "");
-    }
-  );
-});
+  $(document).on("mouseenter", ".fa-sharp.fa-solid.fa-repeat", function () {
+    $(this).css("color", "gold");
+  });
+  $(document).on("mouseleave", ".fa-sharp.fa-solid.fa-repeat", function () {
+    $(this).css("color", "");
+  });
 
-$(document).ready(function () {
-  $(".fa-solid.fa-heart").hover(
-    function () {
-      $(this).css("color", "gold");
-    },
-    function () {
-      $(this).css("color", "");
-    }
-  );
+  $(document).on("mouseenter", ".fa-solid.fa-heart", function () {
+    $(this).css("color", "gold");
+  });
+  $(document).on("mouseleave", ".fa-solid.fa-heart", function () {
+    $(this).css("color", "");
+  });
 });
